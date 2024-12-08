b4f4e83623fdda36c83275344d2fef17a9c0f8ab
svitlana-verkhomii1983 <svetik_hnu@ukr.net>
2024-12-08 21:55:42 +0000

    add changes to file profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
+import "./index.css";
+import App from "./components/App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
